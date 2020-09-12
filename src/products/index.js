fc4e10ea20529eca4145be70bbf7ecca3772b759
fadime94 <fadime.ozdemir11@gmail.com>
2020-09-12 18:01:48 +0300

    added code from repl

--- src/products/index.js
+++ src/products/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Product from '../product'
 
+const arrAllproducts= [{},{}]
+const productObj= {
+  img: "https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9",
+  name: "Cat",
+  description: "A kitty",
+  price: "1.1$"
+}
 const Products = () => {
-    return <Product />
+    return <Product productInfo={productObj} />//map all products and return each product with its prop
 }
 export default Products
