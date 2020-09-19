2415aa4cce59741ee0f8d44ae993c233e2d0d2c9
fadime94 <fadime.ozdemir11@gmail.com>
2020-09-19 14:51:31 +0300

    initial commit

--- src/product/index.js
+++ src/product/index.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import "./index.css";
 
 const Product = (props) => {
-    console.log(props.productInfo.name)
+    // console.log(props)
+    const {name, image, description, models, price, quantity} = props;
+
+    const showModels = ()=>{
+        return models.map((model, index)=><img src={model} alt={`model-${index+1}`}/>)
+    }
+
+    let [likes, setLikes] = React.useState(0)
+    const handleLikeClick = (e)=>{
+        e.preventDefault();
+        setLikes(likes + 1)
+    }
+
+
+    let [inStock, setStock] = React.useState(quantity)
+    
+    const handleBuyClick =(e)=>{
+        e.preventDefault();
+        if(inStock>0) {
+            return setStock(inStock - 1)
+        }
+    }
+
+    let [dislikes, setDislikes]= React.useState(0)
+    const handleDislikeClick = (e)=>{
+        e.preventDefault();
+        setDislikes(dislikes + 1)
+    }
+
+   
+
     return (
-        <>
-             <h2>{props.productInfo.name}</h2>
-        </>
+        <div className="card">
+            <img src={image} alt={ `${name} product image` } />
+            <p>likes:{likes}</p>
+            <button onClick={handleLikeClick}>Like</button>
+            <p>dislikes:{dislikes}</p>
+            <button onClick={handleDislikeClick}>Dislike</button>
+            <h2>{name}</h2>
+            <p>{price}</p>
+            <p>{description}</p>
+            <p>Item in stock:</p>
+            <p>{inStock===0 ? "Out of stock!" : inStock}</p>
+            <button onClick={handleBuyClick} disabled={inStock===0? true : false}>Buy</button>
+            <div className="models">
+            {showModels()} 
+            </div>
+        </div>    
     )
 }
 export default Product
--- src/products/index.js
+++ src/products/index.js
@@ -3,12 +3,22 @@ import Product from '../product'
 
 const arrAllproducts= [{},{}]
 const productObj= {
-  img: "https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9",
+  image: "https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9",
   name: "Cat",
   description: "A kitty",
-  price: "1.1$"
+  models: ["https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9"],
+  price: "1.1$",
+  quantity: 4
 }
+
 const Products = () => {
-    return <Product productInfo={productObj} />//map all products and return each product with its prop
+    return <Product 
+      name={productObj.name}
+      image={productObj.image} 
+      description= {productObj.description}
+      models= {productObj.models}
+      price= {productObj.price}
+      quantity ={productObj.quantity}
+      />//map all products and return each product with its prop
 }
 export default Products
