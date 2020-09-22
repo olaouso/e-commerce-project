c17726d6183b7f5773132003acf7238ce65c0bbd
fadime94 <fadime.ozdemir11@gmail.com>
2020-09-22 20:46:23 +0300

    added database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Products from './products';
+import Navbar from "./navbar/Navbar"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Products />
-    </div>
+      <Router>
+      <Route path="/" component={Navbar} />
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route path="/products" component={Products} />
+      {/* <Route path="/contact" component={Contact} /> */}
+    </Router>
   );
 }
 
--- src/products/index.js
+++ src/products/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import Product from '../product'
 import db from '../firebaseConfig'
+   
+import { Link } from "react-router-dom";
 
 
 
-const arrAllproducts= [{},{}]
-const productObj= {
-  image: "https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9",
-  name: "Cat",
-  description: "A kitty",
-  models: ["https://images.app.goo.gl/2SFZKYbZ8NBgbpEG9"],
-  price: "1.1$",
-  quantity: 4,
-  cart: 0
-}
 
 const Products = () => {
     let [state, setState] = React.useState([]);
@@ -21,25 +13,39 @@ const Products = () => {
     const fetchData = async ()=>{
       const res = await db.collection('products').get();
       const data = res.docs;
-       return data.forEach(el=>console.log("data", el))
-      // return data.map(el=>setState(state=>[...state, el ]))
+      //  return data.forEach(el=>console.log("data", el.data()))
+      return data.map(el=>setState(state=>[...state, el.data() ]))
      
     }
-
+    console.log("state", state)
     React.useEffect(()=>{
       fetchData()
     },[])
 
     // console.log("state",state)
-    
-    return <Product 
-      name={productObj.name}
-      image={productObj.image} 
-      description= {productObj.description}
-      models= {productObj.models}
-      price= {productObj.price}
-      quantity ={productObj.quantity}
-      cart = {productObj.cart}
-      />//map all products and return each product with its prop
+ 
+    return (
+    <div className="products">
+      {
+        state.map((productInfo, index)=>{ 
+          return (
+            
+            <Link to="/product">
+              <Product 
+              name={productInfo.name}
+              image={productInfo.image} 
+              description= {productInfo.description}
+              models= {productInfo.models}
+              price= {productInfo.price}
+              quantity ={productInfo.quantity}
+              cart = {productInfo.cart}
+              id={index}/>
+          </Link>
+          )
+          
+        })
+      }
+    </div>)
+      //map all products and return each product with its prop
 }
 export default Products
