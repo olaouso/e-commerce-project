c40a5e003635d802b3e81fd769bbb0164a186aad
fadime94 <fadime.ozdemir11@gmail.com>
2020-10-13 16:02:28 +0300

    modified navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Products from './products';
 import ProductsList from "./products/productsList"
 import ProductDetails from "./ProductDetails/productDetails";
-import Navbar from "./navbar/Navbar";
+import NavbarMenu from "./navbar/Navbar";
 import Home from "./home/Home"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
       <Router>
-        <Route path="/" component={Navbar} />
+        <Route path="/" component={NavbarMenu} />
         <Route exact path="/" component={Home} />
           
           {/* <Route path='/products' render={routerProps => <ProductsList {...routerProps}/>} /> */}
